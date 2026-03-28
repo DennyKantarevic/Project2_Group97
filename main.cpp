78cd173b610ccc8ed4e75136cc8022101a53d948
nickdevito22 <nicholasdevito123@gmail.com>
2026-03-28 18:43:45 -0400

    Update main.cpp
    fixed missing semicolon in line 97

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ int main(){
   cout << "\nChoose Algorithm:\n";
   cout << "1. Merge Sort\n";
   cout << "2. Heap Sort\n";
-  cout << "3. Comparison\n"
+  cout << "3. Comparison\n";
   cout << "Selection: ";
   cin >> algoChoice;
 
